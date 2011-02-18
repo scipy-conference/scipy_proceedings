9fc4d49bb34078fa1222ad540edc78881a863534
James Bergstra <james.bergstra@gmail.com>
2011-02-18 17:38:37 -0500

    switched order of train and predict in code

--- papers/bergstra/logreg3.py
+++ papers/bergstra/logreg3.py
@@ -1,5 +1,5 @@
+predict = function(inputs=[x], outputs=prediction)
 train = function(
             inputs=[x,y],
             outputs=[prediction, xent],
             updates={w:w-0.1*gw, b:b-0.1*gb})
-predict = function(inputs=[x], outputs=prediction)
