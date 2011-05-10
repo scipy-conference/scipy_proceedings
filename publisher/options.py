ed5b029c620324031ca5673c6c1e40854237c684
Stefan van der Walt <stefan@sun.ac.za>
2011-05-11 00:10:56 +0200

    Refactor config file reader.

--- publisher/options.py
+++ publisher/options.py
@@ -7,9 +7,21 @@ __all__ = ['options']
 from ConfigParser import ConfigParser
 import os.path
 
-options = {}
+default_filename = os.path.join(os.path.dirname(__file__),
+                                '../scipy_proc.cfg')
 
-cp = ConfigParser()
-cp.read(os.path.join(os.path.dirname(__file__), '../scipy_proc.cfg'))
-for key in cp.options('default'):
-    options[key] = cp.get('default', key)
+def cfg2dict(filename=default_filename):
+    """Return the content of a .ini file as a dictionary.
+
+    """
+    options = {}
+
+    cp = ConfigParser()
+    cp.read(filename)
+    for key in cp.options('default'):
+        options[key] = cp.get('default', key)
+
+    return options
+
+options = cfg2dict()
+
