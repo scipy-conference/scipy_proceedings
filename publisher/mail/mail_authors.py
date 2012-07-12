6c9e37c6a7a426ded7a29200ddc1beb18c72aac4
Stefan van der Walt <stefan@sun.ac.za>
2012-07-12 15:51:21 -0700

    Print total # of authors mailed.

--- publisher/mail/mail_authors.py
+++ publisher/mail/mail_authors.py
@@ -8,3 +8,5 @@ config = mailer.load_config('email.json')
 for author in config['authors']:
     to = mailer.email_addr_from(author)
     mailer.send_template(config['sender'], to, args.template, config)
+
+print "Mail for %d authors." % len(config['authors'])
