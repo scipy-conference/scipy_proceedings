a3a88175115cf21b46b43b0857e4890bd28cda57
Stefan van der Walt <stefan@sun.ac.za>
2013-08-18 00:25:19 +0200

    Add 'video' field.
    Conflicts:
    	papers/bergstra_hyperopt/paper.rst

--- publisher/writer/__init__.py
+++ publisher/writer/__init__.py
@@ -37,6 +37,7 @@ class Translator(LaTeXTranslator):
         self.abstract_text = []
         self.keywords = ''
         self.table_caption = []
+        self.video_url = ''
 
         self.abstract_in_progress = False
         self.non_breaking_paragraph = False
@@ -75,6 +76,8 @@ class Translator(LaTeXTranslator):
             self.author_emails.append(text)
         elif self.current_field == 'institution':
             self.author_institutions.append(text)
+        elif self.current_field == 'video':
+            self.video_url = text
 
         self.current_field = ''
 
@@ -160,9 +163,16 @@ class Translator(LaTeXTranslator):
 
         ## Set up title and page headers
 
+        if not self.video_url:
+            video_template = ''
+        else:
+            video_template = r'\\\vspace{5mm}\tt\url{%s}\vspace{-5mm}' % self.video_url
+
         title_template = r'\newcounter{footnotecounter}' \
-                         r'\title{%s}\author{%s}\maketitle'
-        title_template = title_template % (title, ', '.join(authors))
+                r'\title{%s}\author{%s' \
+                r'%s}\maketitle'
+        title_template = title_template % (title, ', '.join(authors),
+                                           video_template)
 
         marks = r'''
           \renewcommand{\leftmark}{%s}
