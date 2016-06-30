scipy-conference/scipy_proceedings

commit 6794ca5885174cd13465d4d7c6d7865949941635

Publisher dies if natbib complains error.
This may be too strict. But it is never bad to be too strict.

This should give an error on #192 (instead of giving question marks)

publisher/build_paper.py
@@ -169,8 +169,7 @@ def tex2pdf_singlepass(out_path):
                 cwd=out_path,
                 )
         out_bib, err = run.communicate()
-
-        if err:
+        if err or 'Error' in out_bib:
             print("Error compiling BiBTeX")
             return out_bib, False
 

commit 2c0a287e74cace75cd565bcdb9754beb67130ee5

Fix Python3 error introduced in 6794ca5

publisher/build_paper.py
@@ -169,7 +169,7 @@ def tex2pdf_singlepass(out_path):
                 cwd=out_path,
                 )
         out_bib, err = run.communicate()
-        if err or 'Error' in out_bib:
+        if err or 'Error' in out_bib.decode():
             print("Error compiling BiBTeX")
             return out_bib, False
 