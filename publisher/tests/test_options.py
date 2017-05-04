23b8c55e905a6e418a6ed8d53f989a1f52d8feca
mpacer <michaelpacer@gmail.com>
2017-05-03 19:25:12 -0700

    add some initial tests for most problematic json functions

---
+++ publisher/tests/test_options.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals, print_function
+
+import os
+
+import options 
+
+from testpath import tempdir
+
+def test_dict2cfg():
+    d = {'title': 'temp_title'}
+    with tempdir.TemporaryDirectory() as td:
+        loc_path = os.path.join(td, 'paper_stats.json')
+        options.dict2cfg(d, loc_path)
+
+
+def test_cfg2dict():
+    d = {'title': 'temp_title'}
+    with tempdir.TemporaryDirectory() as td:
+        loc_path = os.path.join(td, 'paper_stats.json')
+        options.dict2cfg(d, loc_path)
+        test_d = options.cfg2dict(loc_path)
+        assert test_d == d
