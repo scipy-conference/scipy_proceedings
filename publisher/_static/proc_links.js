3f364be11347d3a039481978859eb11e6e010313
M Pacer <mpacer@berkeley.edu>
2017-07-17 19:40:01 -0700

    actually use javascript from local file at same directory level

--- publisher/_static/proc_links.js
+++ publisher/_static/proc_links.js
@@ -1,5 +1,5 @@
 function proc_versions() {
-   var versions = ['2011', '2010', '2009', '2008'];
+   var versions = range(2017, 2008)
    var proc_url = 'http://conference.scipy.org/proceedings/scipy';
    document.write('<ul id="navibar">');
 
@@ -10,4 +10,39 @@ function proc_versions() {
    }
 
    document.write('</ul>');
+}
+var range = function(start, end, step) {
+    var range = [];
+    var typeofStart = typeof start;
+    var typeofEnd = typeof end;
+
+    if (step === 0) {
+        throw TypeError("Step cannot be zero.");
+    }
+
+    if (typeofStart == "undefined" || typeofEnd == "undefined") {
+        throw TypeError("Must pass start and end arguments.");
+    } else if (typeofStart != typeofEnd) {
+        throw TypeError("Start and end arguments must be of same type.");
+    }
+
+    typeof step == "undefined" && (step = 1);
+
+    if (end < start) {
+        step = -step;
+    }
+
+    if (typeofStart == "number") {
+
+        while (step > 0 ? end >= start : end <= start) {
+            range.push(start);
+            start += step;
+        }
+
+    } else {
+        throw TypeError("Only number types are supported");
+    }
+
+    return range;
+
 }
