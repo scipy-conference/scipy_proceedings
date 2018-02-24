ee0a72366ac55fcc42e02b9c04fb6ec04d02c72e
M Pacer <mpacer@berkeley.edu>
2018-02-24 13:34:44 -0800

    better js code; accommodate strings, document range function

--- publisher/_static/proc_links.js
+++ publisher/_static/proc_links.js
@@ -1,5 +1,5 @@
 function proc_versions() {
-   var versions = range(2017, 2008)
+   var versions = range('2017', '2008')
    var proc_url = 'http://conference.scipy.org/proceedings/scipy';
    document.write('<ul id="navibar">');
 
@@ -11,38 +11,50 @@ function proc_versions() {
 
    document.write('</ul>');
 }
+
+// range(start, end, step) 
+// provides a range of integers or strings, depending on the type of input provided.
+// If you input strings they will be converted to integers for the iteration logic to work.
+// start: Int or string, first element to include in output
+// end: Int or string, last element to include in output
+// step: Int, amount and direction the array will be stepped through
 var range = function(start, end, step) {
-    var range = [];
-    var typeofStart = typeof start;
-    var typeofEnd = typeof end;
+  var range = [];
+  var typeOfStart = typeof start;
+  var typeOfEnd = typeof end;
 
-    if (step === 0) {
-        throw TypeError("Step cannot be zero.");
+  if (typeOfStart == "undefined" || typeOfEnd == "undefined") {
+    throw TypeError("Must pass start and end arguments.");
+  } else if (typeOfStart != typeOfEnd) {
+    throw TypeError("Start and end arguments must be of same type.");
+  }
+  if (typeOfStart == "string"){ 
+    start = parseInt(start)
+    end = parseInt(end)
+  } 
+  
+  if (step === 0) {
+    throw TypeError("Step cannot be zero.");
+  }
+  typeof step == "undefined" && (step = 1);
+
+  if (end < start) {
+    step = -step;
+  }
+
+  if (typeOfStart == "number") {
+    while (step > 0 ? end >= start : end <= start) {
+      range.push(start);
+      start += step;
     }
-
-    if (typeofStart == "undefined" || typeofEnd == "undefined") {
-        throw TypeError("Must pass start and end arguments.");
-    } else if (typeofStart != typeofEnd) {
-        throw TypeError("Start and end arguments must be of same type.");
-    }
-
-    typeof step == "undefined" && (step = 1);
-
-    if (end < start) {
-        step = -step;
-    }
-
-    if (typeofStart == "number") {
-
-        while (step > 0 ? end >= start : end <= start) {
-            range.push(start);
-            start += step;
-        }
-
-    } else {
-        throw TypeError("Only number types are supported");
-    }
-
-    return range;
-
+  } else if (typeOfStart == "string") {
+      while (step > 0 ? end >= start : end <= start) {
+        range.push(start.toString());
+        start += step;
+      }
+  } else {
+    throw TypeError("Only number and string types are supported");
+  }
+  
+  return range;
 }
