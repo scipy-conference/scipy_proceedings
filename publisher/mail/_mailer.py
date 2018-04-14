6924d4aa89e2b317a42e0fdafd7a182751330f93
M Pacer <mpacer@berkeley.edu>
2018-04-13 23:30:15 -0700

    update mailer to provide common routines for creating common info

--- publisher/mail/_mailer.py
+++ publisher/mail/_mailer.py
@@ -6,7 +6,8 @@ from email.mime.text import MIMEText
 
 import sys
 sys.path.insert(0, '..')
-from conf import work_dir
+
+from conf import proc_conf
 from options import cfg2dict
 from build_template import _from_template
 
@@ -14,11 +15,13 @@ from build_template import _from_template
 args = None
 password = None
 
+
 def author_greeting(names):
     if len(names) == 1:
         return names[0]
     else:
         return ', '.join(names[:-1]) + ', and ' + names[-1]
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Invite reviewers.")
@@ -49,6 +52,40 @@ def email_addr_from(name_email):
     return '"%s" <%s>' % (name_email['name'], name_email['email'])
 
 
+def create_committee(data=None):
+    if data is None:
+        data = cfg2dict(proc_conf)
+    proc = data.get('proceedings', {})
+    editors = proc.get('editor', [])
+    # editor_email = [x.strip() for x in proc.get('editor_email', '').split(',')]
+    editor_email = proc.get('editor_email')
+    assert len(editors) == len(editor_email)
+    return [{"name": name, 
+             "email": email} 
+            for name, email 
+            in zip(editors, editor_email)]
+
+def editor_email_string(data=None):
+    if data is None:
+        data = cfg2dict(proc_conf)
+    editor_email_list = data.get('proceedings', {}).get('editor_email',[])
+    return ', '.join(editor_email_list)
+
+def create_message(recipient, template, template_data):
+    template_data['email'] = recipient
+    template_path = resolve_template(template, '../mail/templates/')
+    return _from_template(template_path, template_data)
+
+
+def resolve_template(template_name, base_dir):
+    template_path = os.path.join(base_dir, template_name)
+    # This needs to be add '.tmpl' because that's what _from_template expects
+    if not os.path.exists(template_path +'.tmpl'):
+        raise ValueError('There is no template file at {}.'.format(template_path+'.tmpl'))
+    else:
+        return os.path.abspath(template_path)
+
+
 def send_template(sender, recipient, template, template_data,
                   smtp_server='smtp.gmail.com', smtp_port=587):
     if args.dry_run:
@@ -56,15 +93,13 @@ def send_template(sender, recipient, template, template_data,
     else:
         get_password(sender['login'])
         print('-> %s' % recipient)
-
-    template_data['email'] = recipient
-    message = _from_template('../mail/templates/' + template, template_data)
+    
+    message = create_message(recipient, template, template_data)
 
     if args.dry_run:
         print("=" * 80)
         print(message)
         print("=" * 80)
-
         return
 
     session = smtplib.SMTP(smtp_server, smtp_port)
