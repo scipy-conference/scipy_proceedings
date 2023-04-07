3aa0889dacb0a0ed979adbc214f83d221e97ff18
Dillon Niederhut <dillon.niederhut@gmail.com>
2023-04-06 23:51:24 -0500

    ENH: permit latex inputs w/ docutils builds
    Alternate proposal to changes in #679 that includes support
    for latex paper inputs, but without switching out docutils for
    pandoc. Requires some changes to tempita and the html templates
    to correctly handle raw latex inputs.

--- publisher/tempita/__init__.py
+++ publisher/tempita/__init__.py
@@ -466,10 +466,10 @@ def html_quote(value, force=True):
     if not isinstance(value, basestring_):
         value = coerce_text(value)
     if sys.version >= "3" and isinstance(value, bytes):
-        value = cgi.escape(value.decode('latin1'), 1)
+        value = html_escape(value.decode('latin1'), 1)
         value = value.encode('latin1')
     else:
-        value = cgi.escape(value, 1)
+        value = html_escape(value, 1)
     if sys.version < "3":
         if is_unicode(value):
             value = value.encode('ascii', 'xmlcharrefreplace')
