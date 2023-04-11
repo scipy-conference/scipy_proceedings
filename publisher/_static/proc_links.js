032ced156cd7744a2d0f4978e5be5b3c096ff96e
Dillon Niederhut <dillon.niederhut@gmail.com>
2023-04-10 22:15:35 -0500

    ENH: update http to https in headers
    Also add recent years to proc_link

--- publisher/_static/proc_links.js
+++ publisher/_static/proc_links.js
@@ -1,6 +1,6 @@
 function proc_versions() {
-   var versions = ['2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008'];
-   var proc_url = 'http://conference.scipy.org/proceedings/scipy';
+   var versions = ['2023', '2022', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008'];
+   var proc_url = 'https://conference.scipy.org/proceedings/scipy';
    document.write('<ul id="navibar">');
 
    for (i=0; i < versions.length; i++) {
