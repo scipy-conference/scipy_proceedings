fe92e3cc717e261a6184e98db2dbb289a2832699
Matthew Feickert <matthew.feickert@cern.ch>
2024-08-21 00:02:35 -0500

    style: Apply prettier changes
    * Also fix the following errors:
    
    publisher/_static/scipy-proc.css
    [error] publisher/_static/scipy-proc.css: SyntaxError: CssSyntaxError: Missed semicolon (28:23)
    [error]   26 | .abstract {
    [error]   27 |    width: 80ex;
    [error] > 28 |    text-align: justify
    [error]      |                       ^
    [error]   29 |    padding-left: 1em;
    [error]   30 | }
    [error]   31 |
    publisher/_static/screen.css
    [error] publisher/_static/screen.css: SyntaxError: CssSyntaxError: Unknown word (57:3)
    [error]   55 |   float: right;
    [error]   56 |   margin: 0.5ex 10px 0.5ex 0px;
    [error] > 57 |   padding 0;
    [error]      |   ^
    [error]   58 |   display: inline; /* forces IE to calc correct margin */
    [error]   59 | }
    [error]   60 |

--- publisher/_static/proc_links.js
+++ publisher/_static/proc_links.js
@@ -1,13 +1,31 @@
 function proc_versions() {
-   var versions = ['2023', '2022', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008'];
-   var proc_url = 'https://conference.scipy.org/proceedings/scipy';
-   document.write('<ul id="navibar">');
+  var versions = [
+    "2023",
+    "2022",
+    "2020",
+    "2019",
+    "2018",
+    "2017",
+    "2016",
+    "2015",
+    "2014",
+    "2013",
+    "2012",
+    "2011",
+    "2010",
+    "2009",
+    "2008",
+  ];
+  var proc_url = "https://conference.scipy.org/proceedings/scipy";
+  document.write('<ul id="navibar">');
 
-   for (i=0; i < versions.length; i++) {
-       document.write('<li class="wikilink">');
-       document.write('<a href="' + proc_url + versions[i] + '">SciPy ' + versions[i] +  '</a>');
-       document.write('</li>');
-   }
+  for (i = 0; i < versions.length; i++) {
+    document.write('<li class="wikilink">');
+    document.write(
+      '<a href="' + proc_url + versions[i] + '">SciPy ' + versions[i] + "</a>",
+    );
+    document.write("</li>");
+  }
 
-   document.write('</ul>');
+  document.write("</ul>");
 }
