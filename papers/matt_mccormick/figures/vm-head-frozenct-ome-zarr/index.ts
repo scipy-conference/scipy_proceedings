a4078db151aeee4e17e63ea3269e12ebce554970
Matt McCormick <matt.mccormick@kitware.com>
2024-08-27 21:45:31 -0400

    Set normalizedOpacityPoints

--- papers/matt_mccormick/figures/vm-head-frozenct-ome-zarr/index.ts
+++ papers/matt_mccormick/figures/vm-head-frozenct-ome-zarr/index.ts
@@ -39,4 +39,8 @@ document.addEventListener("DOMContentLoaded", async function () {
       ]),
     },
   });
+
+  setTimeout(() => {
+    imageActor.send({ type: "normalizedOpacityPoints", points: [[0.25, 0.05], [0.5, 0.7], [0.8, 1.0]], component: 0 });
+  }, 1500);
 });
